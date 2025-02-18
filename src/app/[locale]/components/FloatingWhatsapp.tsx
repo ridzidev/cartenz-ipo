2002739bc13dbb14ade90ef771137249f622442f
alfa.ridzi <alfa.ridzi@cartenz.co.id>
2025-02-18 15:51:38 +0700

    vsts 4 whatsapp

--- src/app/[locale]/components/FloatingWhatsapp.tsx
+++ src/app/[locale]/components/FloatingWhatsapp.tsx
@@ -3,7 +3,7 @@ const FloatingWhatsapp = () => {
   return (
     <div className='fixed bottom-10 right-10 z-[1000] flex h-16 w-16 transform items-center justify-center rounded-full bg-green-500 p-2 text-4xl text-white shadow-md transition-transform hover:scale-110 hover:bg-green-600'>
       <a
-        href='https://wa.me/6281234567890'
+        href='https://wa.me/6282127082062'
         target='_blank'
         rel='noopener noreferrer'
         className=''
